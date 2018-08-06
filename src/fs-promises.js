d27c880caff152b60f5bf46eb21e3e97a967e362
staskorz <staskorz@gmail.com>
2018-08-06 21:22:07 +0300

    Added promisified readFile method

--- src/fs-promises.js
+++ src/fs-promises.js
@@ -2,7 +2,9 @@ const { promisify } = require("util")
 const fs = require("fs")
 
 const readDirPromise = promisify(fs.readdir)
+const readFilePromise = promisify(fs.readFile)
 
 module.exports = {
   readDirPromise,
+  readFilePromise,
 }

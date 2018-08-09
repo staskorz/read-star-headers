c73ee88f549d7d5f821f6f52cc3124131d524fa0
staskorz <staskorz@gmail.com>
2018-08-09 17:06:33 +0300

    Added logging

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@
 require("dotenv").config()
 
 const Rx = require("rxjs")
-const { map, mergeMap, toArray } = require("rxjs/operators")
+const { map, mergeMap, toArray, tap, scan } = require("rxjs/operators")
 const fs = require("fs")
 const path = require("path")
 const { unparse: jsonToCSV } = require("papaparse")
 
 const parseStarHeader = require("./parse-star-header")
+
+const LOG_NTH_LINE = 1000
 
 const { SRC_DIRS, DST } = process.env
 
@@ -18,6 +20,15 @@ const readDirAsObservable = Rx.bindNodeCallback(fs.readdir)
 const readFileAsObservable = Rx.bindNodeCallback(fs.readFile)
 
 console.log(`Processing STAR directories: ${dirs.join("; ")}`)
+
+const loggingObserver = new Rx.Subject()
+loggingObserver.pipe(scan(count => count + 1, 0)).subscribe({
+  next: count => {
+    if (count % LOG_NTH_LINE === 0) {
+      console.log(`Processed ${count.toLocaleString("en-US")} files`)
+    }
+  },
+})
 
 Rx.from(dirs)
   .pipe(
@@ -44,9 +55,12 @@ Rx.from(dirs)
         map(content => ({ ...fileObject, ...content })),
       ),
     ),
+    tap(() => loggingObserver.next(1)),
     toArray(),
   )
   .subscribe(json => {
+    console.log(`Lines: ${json.length}`)
+
     const csv = jsonToCSV(json)
 
     console.log(`Writing to ${DST}`)
